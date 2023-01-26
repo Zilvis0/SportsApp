989cb1401ccfb8b39c02c570aaa65e34192db264
Zilvinas <zilvinas.pusnys@gmail.com>
2023-01-26 19:41:45 +0200

    Starting implement carousel idea for Hero

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import "./SectionHero.scss";
 
 export default function SectionHero() {
-  return <div>SectionHero</div>;
+  const newArr = [1, 2, 3, 4, 5];
+  return (
+    <div className="frame__wrapper">
+      {newArr.map((item) => (
+        <div className="frame">{item}</div>
+      ))}
+    </div>
+  );
 }
