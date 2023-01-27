b553baacd626a38abf62de9046e53080af2992f9
Zilvinas <zilvinas.pusnys@gmail.com>
2023-01-27 11:52:57 +0200

    Improved input more components colors as in style

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Card.scss";
+
+export default function Card() {
+  return (
+    <div className="card__wrapper">
+      <h1 className="card">hello</h1>
+    </div>
+  );
+}
--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import "./SectionHero.scss";
+import Card from "../Card";
 
 export default function SectionHero() {
   const newArr = [1, 2, 3, 4, 5];
   return (
-    <div className="frame__wrapper">
-      {newArr.map((item) => (
-        <div className="frame">{item}</div>
-      ))}
+    <div className="section-hero__wrapper">
+      <section className="section-hero">
+        <div className="section-hero__input-wrapper">
+          <div className="section-hero__input-svg">SVG</div>
+          <input className="section-hero__input" placeholder="Search"></input>
+          <button className="section-hero__input-button">Go!</button>
+        </div>
+        <div className="section-hero__cards">
+          {newArr.map((item) => (
+            <Card>{item}</Card>
+          ))}
+        </div>
+      </section>
     </div>
   );
 }
