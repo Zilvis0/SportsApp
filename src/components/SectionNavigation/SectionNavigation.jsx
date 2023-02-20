94056619d5b122e4514043d11a4441f4d55142d4
Zilvinas <zilvinas.pusnys@gmail.com>
2023-02-20 09:04:44 +0200

    Changed hero section hover animation, fixed logo

--- src/components/SectionNavigation/SectionNavigation.jsx
+++ src/components/SectionNavigation/SectionNavigation.jsx
@@ -4,7 +4,10 @@ import "./SectionNavigation.scss";
 export default function SectionNavigation() {
   return (
     <nav className="section-navigation">
-      <div className="section-navigation__logo">LogoPlaceholder</div>
+      <div className="section-navigation__logo">
+        <img src="./src/assets/logo.svg"></img>
+        <div className="section-navigation__logo-text">SportsApp</div>
+      </div>
       <menu className="section-navigation__menu">
         <div className="section-navigation__option">Test</div>
         <div className="section-navigation__option">Test2</div>
