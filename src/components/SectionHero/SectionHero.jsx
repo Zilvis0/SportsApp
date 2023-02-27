3ff1aaa2312d740a19481402b3e6f4c96272f358
Zilvinas <zilvinas.pusnys@gmail.com>
2023-02-27 13:54:25 +0200

    Improved exercise display

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../Card";
 import "./SectionHero.scss";
 
 export default function SectionHero() {
-  const newArr = [1, 2, 3, 4, "Test"];
+  const [search, setSearch] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  // const [exerciseArray, setExerciseArray] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSearch = () => {
+    setSearch(inputValue);
+    setInputValue("");
+  };
+
+  // useEffect(() => {
+  //   $.ajax({
+  //     method: "GET",
+  //     url: "https://api.api-ninjas.com/v1/exercises?muscle=" + search,
+  //     headers: { "X-Api-Key": "4nmeW2NeFxKcC9W+1O2XrQ==QtmbDMGWsjTTaKTb" },
+  //     contentType: "application/json",
+  //     success: function (result) {
+  //       console.log(result);
+  //       setExerciseArray(result);
+  //     },
+  //     error: function ajaxError(jqXHR) {
+  //       console.error("Error: ", jqXHR.responseText);
+  //     },
+  //   });
+  // }, [search]);
+
+  const newArr = [1, 2, 3, 4, "Test", 1, 2, 3, 4, "Test"];
   return (
     <div className="section-hero__wrapper">
       <section className="section-hero">
@@ -12,10 +42,20 @@ export default function SectionHero() {
             src="./src/assets/logo.svg"
             className="section-hero__input-svg"
           ></img>
-          <input className="section-hero__input" placeholder="Search"></input>
-          <button className="section-hero__input-button">Go!</button>
+          <input
+            onChange={handleInputChange}
+            className="section-hero__input"
+            placeholder="Search"
+            value={inputValue}
+          ></input>
+          <button onClick={handleSearch} className="section-hero__input-button">
+            Go!
+          </button>
         </div>
         <div className="section-hero__cards">
+          {/* {exerciseArray && exerciseArray.length > 0
+            ? exerciseArray.map((item, i) => <Card key={i}>{item.name}</Card>)
+            : null} */}
           {newArr.map((item, i) => (
             <Card key={i}>{item}</Card>
           ))}
