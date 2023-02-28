969a53694eea222df14821c6200c4931df9c3345
Zilvinas <zilvinas.pusnys@gmail.com>
2023-02-28 17:37:44 +0200

    added some global variables

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import "./Card.scss";
 
 export default function Card({ children }) {
+  const handleCapitalL = (string) => {
+    let firstLetter = string[0].toUpperCase();
+    let remainingLetters = string.slice(1);
+    return firstLetter + remainingLetters;
+  };
+
+  const extractedChildren =
+    typeof children === "string"
+      ? handleCapitalL(children.replace("_", " "))
+      : children;
+
   return (
     <div className="card__wrapper">
       <img src="./src/assets/random-exercise.jpg" alt="exercise" />
-      <h1 className="card">{children}</h1>
+      <h2 className="card">{extractedChildren}</h2>
     </div>
   );
 }
